33ba888a09ebcd7630e001a4aa523c9024e28f8b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-01-16 20:36:43 -0500

    sample

--- sample.sh
+++ sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o xtrace         # print commands as they run
-set -o errexit        # exit immediately if a pipeline returns a non-zero status
+# set -o errexit        # exit immediately if a pipeline returns a non-zero status
 set -o errtrace       # trap ERR from shell functions, command substitutions, and commands from subshell
 set -o nounset        # treat unset variables as an error
 set -o pipefail       # pipe will exit with last non-zero status if applicable
@@ -8,11 +8,12 @@ set -o pipefail       # pipe will exit with last non-zero status if applicable
 # set -o monitor      # job control is enabled
 # set -o noclobber    # prevent output redirection using '>', '>&', '<>' from overwriting existing files
 
-TABS=tabs_recent.txt
+TABS=$1
+# TABS=~/d/00_Metadata/tabs_recent.txt
 # TABS=tabs.txt
 
 WIN=~/d/35_Linux_Software/bin/wsl-open.sh
 UNX=xdg-open
 
-head -7 ~/d/00_Metadata/$TABS | xargs -n1 -P 12 -t sh $UNX
-sed -i -e 1,7d ~/d/00_Metadata/$TABS
+head -7 $TABS | xargs -n1 -P 12 -t sh $UNX
+sed -i -e 1,7d $TABS
